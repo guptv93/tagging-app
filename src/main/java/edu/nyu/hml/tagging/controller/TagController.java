af68e409305ebcd660cc98e8cd4f4d64d52a6c3e
Vaibhav Gupta <guptv.93@gmail.com>
2019-10-09 22:14:19 -0400

    Tags Done and Dusted

--- src/main/java/edu/nyu/hml/tagging/controller/TagController.java
+++ src/main/java/edu/nyu/hml/tagging/controller/TagController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,19 +27,28 @@ public class TagController {
         return new ModelAndView("tags", model);
     }
 
-    @ResponseBody
     @PostMapping("/tags/{folderName}")
-    public void addTags(@PathVariable("folderName") String folderName, @RequestBody String tagString) {
+    public ModelAndView addTags(Map<String, Object> model,
+            @PathVariable("folderName") String folderName,
+            @RequestBody String tagString) {
         String[] tags = tagString.split(",");
         List<String> cleanTags = new ArrayList<>();
-        for(String tag: tags) {
+        for (String tag : tags) {
             tag = tag.trim();
-            if(!tag.isEmpty()) {
+            if (!tag.isEmpty()) {
                 cleanTags.add(tag);
             }
         }
         tagService.addTags(folderName, cleanTags);
-        System.out.println(tagService.getTags(folderName));
+        model.put("tags", tagService.getTags(folderName));
+        return new ModelAndView("tags", model);
+    }
+
+    @ResponseBody
+    @PutMapping("/tags/{folderName}")
+    public void updateTags(@PathVariable("folderName") String folderName,
+            @RequestBody List<String> tags) {
+        tagService.addTags(folderName, tags);
     }
 
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,7 +3,10 @@ var app = {
     app.folderName = $("#folder-name").text();
     app.currentIndex = $("#image-index").text();
     app.insertImage(app.currentIndex);
-    $('.my-arrow-parent').on('click', app.changeImage);
+    app.getTags();
+    $('.my-arrow-parent').on('click', app.onArrowClick);
+    $('#tags-div').on('click', '#add-tags-button', app.sendTags);
+    $('#tags-div').on('click', '#refresh-tags-button', app.getTags);
   },
 
   insertImage : function(imgId) {
@@ -25,9 +28,24 @@ var app = {
     });
   },
 
-  changeImage : function(event) {
+  onArrowClick : function(event) {
     event.preventDefault();
+    if ($("#user-id-input").val() == "") {
+      $("#user-id-input").addClass("is-invalid");
+      return;
+    } else {
+      $("#user-id-input").removeClass("is-invalid");
+    }
     if(this.id == "prev") {
+      app.changeIndex(false);
+    } else {
+      app.changeIndex(true);
+    };
+    app.updateTags(app.selectedTags());
+  },
+
+  changeIndex : function(isNext) {
+    if(isNext == false) {
       if(app.currentIndex == 1) {
         alert("This is the first image in the folder!");
       } else {
@@ -37,9 +55,57 @@ var app = {
       app.currentIndex++;
     }
     app.insertImage(app.currentIndex);
+  },
+
+  getTags : function() {
+    url = "/tags/" + app.folderName;
+    $.ajax(url, {
+      type : 'GET',
+      success : app.tagsSuccessCallback,
+      error : app.tagsErrorCallback
+    });
+  },
+
+  sendTags : function(payload) {
+    url = "/tags/" + app.folderName;
+    $.ajax(url, {
+      type : 'POST',
+      data : $('#add-tags-input').val(),
+      contentType : 'application/json',
+      success : app.tagsSuccessCallback,
+      error : app.tagsErrorCallback
+    });
+  },
+
+  updateTags : function(payload) {
+    url = "/tags/" + app.folderName;
+    $.ajax(url, {
+      type : 'PUT',
+      data : JSON.stringify(payload),
+      contentType : 'application/json'
+    });
+  },
+
+  selectedTags : function() {
+    var tags = [];
+    $('input').each(function() {
+      $this = $(this);
+      if ( $this.prop('checked') ) {
+        tags.push($this.closest('div').find('label').html());
+      }
+    });
+    return tags;
+  },
+
+  tagsSuccessCallback : function(result) {
+    $tagsDiv = $('#tags-div');
+    $tagsDiv.html(result);
+  },
+
+  tagsErrorCallback : function(request, errorType, errorMessage) {
+    alert(request.responseJSON.message);
   }
 }
-
 
 $(document).ready(function(){
   app.init();
